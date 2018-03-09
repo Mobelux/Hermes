d9f9079a1093ed4047e030985da5987b43f355b8
Jeremy Greenwood <jeremy@mobelux.com>
2018-03-09 15:40:22 -0500

    added actioncable target

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Hermes",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Hermes",
-            targets: ["Hermes"])
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.0")
@@ -18,11 +12,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Hermes",
-            dependencies: ["Starscream"]),
-        .testTarget(
-            name: "HermesTests",
-            dependencies: ["Hermes"])
+        .target(name: "Hermes", dependencies: ["Starscream"]),
+        .target(name: "ActionCable", dependencies: ["Hermes"]),
+        .testTarget(name: "HermesTests", dependencies: ["Hermes"]),
+        .testTarget(name: "ActionCableTests", dependencies: ["ActionCable"])
     ]
 )
---
+++ Sources/ActionCable/ActionCable.swift
@@ -0,0 +1,15 @@
+//
+//  ActionCable.swift
+//  ActionCable
+//
+//  Created by Jeremy Greenwood on 3/9/18.
+//
+
+public typealias SubscribedHandler = () -> ()
+public typealias UnsubscribedHandler = () -> ()
+public typealias RejectedHandler = () -> ()
+public typealias ReceivedHandler = () -> ()
+
+public class ActionCable {
+
+}
