c94d378aa5720710ee94d68b4d459d50f1587720
Jeremy Greenwood <jeremy@mobelux.com>
2018-03-14 15:17:18 -0400

    added starscream sub and real proj

---
+++ Sources/ActionCable/ActionCable.h
@@ -0,0 +1,19 @@
+//
+//  ActionCable.h
+//  ActionCable
+//
+//  Created by Jeremy Greenwood on 3/14/18.
+//  Copyright © 2018 Mobelux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionCable.
+FOUNDATION_EXPORT double ActionCableVersionNumber;
+
+//! Project version string for ActionCable.
+FOUNDATION_EXPORT const unsigned char ActionCableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionCable/PublicHeader.h>
+
+
